bcf1a8158118d4960b9ae283e0513e268a82d64e
George Timoschenko <malfermu@malfer.mu>
2013-01-12 01:39:32 +0400

    Huh?

---
+++ code/existed.rb
@@ -0,0 +1,18 @@
+# encoding: utf-8
+
+class Existed 
+  def initialize
+    $ready = redis.get 'ready'
+    if $ready
+      $title = redis.get 'title'
+      $description = redis.get 'description'
+      $author = redis.get 'author'
+      $articles = []
+      $articles << redis.get("mykey")
+    end
+  end
+
+  def load page = 1
+
+  end
+end
