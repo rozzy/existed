c3294f842cf094918d034c2133641cc989e993af
George Timoschenko <malfermu@malfer.mu>
2013-01-20 03:23:21 +0400

    Smth.

--- app.rb
+++ app.rb
@@ -3,7 +3,7 @@ get '/*.css' do |css|
   style = "#{settings.views}/#{css}.css"
   return File.read(style) if File.exists?(style)
   sass :"#{css}", Compass.sass_engine_options
-    .merge(output: :compressed) 
+    .merge(output: :compressed)
 end
 
 get '/' do
